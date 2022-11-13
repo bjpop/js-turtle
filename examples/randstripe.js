b214db1449a70434eac942e109e326d432817dcd
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-13 15:32:54 -0400

    Update randstripe.js

--- examples/randstripe.js
+++ examples/randstripe.js
@@ -1,19 +1,16 @@
 // draw some randomly placed coloured stripes
 
 function fun(count) {
-   while (count-- > 0)
-   {
-      const
-         x = random(-150, 150),
-         y = random(-150, 150);
-      goto(x,y);
-      const
-         r = random(0, 255),
-         g = random(0, 255),
-         b = random(0, 255);
-      colour(r, g, b, Math.random());
-      angle(random(0, 180));
-      width(random(1, 10));
+   while (count-- > 0) {
+      goto(random(-150, 150), random(-150, 150));
+      colour(
+         random(0,255),
+         random(0,255),
+         random(0,255),
+         Math.random()
+      );
+      angle(random(0,180));
+      width(random(1,10));
       forward(random(10, 30));
    }
 }
