ca15abfcdb9acfa654e3703d9c88bcee7868284e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-13 16:35:05 -0400

    convert `part` in `halfSierpinski` to global pure fn
    also renamed `part` in `sierpinski` to `half` to avoid name collision, and for "more accurate" self-commenting code

--- examples/sierpinski.js
+++ examples/sierpinski.js
@@ -1,35 +1,37 @@
 // https://en.wikipedia.org/wiki/Sierpi%C5%84ski_curve
 
+function part(size, level) {
+   level--;
+   halfSierpinski(size, level);
+   left(45);
+   forward(size * Math.SQRT2);
+   left(45);
+   halfSierpinski(size, level);
+}
+
 /** helper for `sierpinski` */
 function halfSierpinski(size, level) {
    if (level <= 0) {
       forward(size);
       return;
    }
-   const part = () => {
-      halfSierpinski(size, level - 1);
-      left(45);
-      forward(size * Math.SQRT2);
-      left(45);
-      halfSierpinski(size, level - 1);
-   };
-   part();
+   part(size, level);
    right(90);
    forward(size);
    right(90);
-   part();
+   part(size, level);
 }
 
 /** draw a Sierpinski Curve of arbitrary recursive depth */
 function sierpinski(size, level) {
-   const part = () => {
+   function half() {
       halfSierpinski(size, level);
       right(90);
       forward(size);
       right(90);
-   };
-   part();
-   part();
+   }
+   half();
+   half();
 }
 
 function demo() {
